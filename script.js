ee0417f4f992ec6748ee1aff439c0390ed66aae4
Makktu <johnmcna@gmail.com>
2022-02-01 18:28:51 +0000

    Some of it

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 "use strict";
+
+const container = document.querySelector(".container");
+
+const newBtn = document.querySelector(".new-file");
+
+newBtn.addEventListener("click", () => {
+    console.log("New File clicked");
+});
